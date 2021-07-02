0577f1866f57d42fe5334fe81e36a5903c8b95cc
saumyaparekh12 <84619774+saumyaparekh12@users.noreply.github.com>
2021-07-02 19:11:55 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,18 @@
+class Polygon {
+    constructor(x, y, width, height){
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+  
+        }
+       
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+  display(){
+    rectMode(CENTER);
+   // rect(pos.x,pos.y,this.width, this.height);  
+  }
+  };
